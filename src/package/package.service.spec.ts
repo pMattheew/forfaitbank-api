b38f963b98999b6d6e020a3fb30a5503ad209712
Patrick <99044724+pMattheew@users.noreply.github.com>
2023-01-10 12:49:08 +0000

    createMany method implemented with tests

--- src/package/package.service.spec.ts
+++ src/package/package.service.spec.ts
@@ -1,15 +1,16 @@
+import { CreateManyPackagesDto } from "./dto/create-many-packages.dto"
+import { CreatePackageDto } from "./dto/create-package.dto"
+import { ConfigService } from "@nestjs/config"
+import { TestingModule, Test } from "@nestjs/testing"
+import { PrismaService } from "./../prisma/prisma.service"
+import { PackageService } from "./package.service"
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { CreatePackageDto } from './dto/create-package.dto';
-import { ConfigService } from "@nestjs/config";
-import { TestingModule, Test } from "@nestjs/testing";
-import { PrismaService } from "./../prisma/prisma.service";
-import { PackageService } from "./package.service";
 
 describe("PackageService", () => {
-   let service: PackageService;
-   let prisma: PrismaService;
-   let packageColor: string;
-
+   let service: PackageService
+   let prisma: PrismaService
+   let grandpaOperationId: number
+   let operationId: number
 
    beforeAll(async () => {
       const module: TestingModule = await Test.createTestingModule({
@@ -17,39 +18,87 @@ describe("PackageService", () => {
             PackageService,
             PrismaService,
             PackageService,
-            ConfigService,
-         ],
-      }).compile();
-      service = module.get<PackageService>(PackageService);
-      prisma = module.get<PrismaService>(PrismaService);
-   });
+            ConfigService
+         ]
+      }).compile()
+      service = module.get<PackageService>(PackageService)
+      prisma = module.get<PrismaService>(PrismaService)
+   })
 
    afterAll(async () => {
-      await prisma.cleanDatabase();
-   });
+      await prisma.cleanDatabase()
+   })
 
-   describe("should create", () => {
+   describe("create", () => {
       const dto: CreatePackageDto = {
          billType: 10,
-         billQuantity: 50,
+         billQuantity: 50
       }
+      let packageColor: string
 
-      it("package closed", async () => {
+      it("should create closed package", async () => {
          const pkg = await service.create(dto)
          packageColor = pkg.color
 
-         expect(pkg.status).toBe('closed')
+         expect(pkg.status).toBe("closed")
          expect(pkg.color).toEqual(expect.any(String))
       })
-      it("package opened with distinct color", async () => {
+
+      it("should create opened package with distinct color", async () => {
          dto.billQuantity = 45
 
          const pkg = await service.create(dto)
-         
-         expect(pkg.status).toBe('opened')
+
+         expect(pkg.status).toBe("opened")
          expect(pkg.color).not.toBe(packageColor)
+         operationId = pkg.operationId
       })
-      it("many closed packages", async () => {})
-      it("many closed packages with last opened", async () => {})
+   })
+
+   describe("createMany", () => {
+      const manyDto: CreateManyPackagesDto = {
+         billType: 100,
+         value: 100,
+         grandpaId: 1
+      }
+
+      it("should create only one closed package", async () => {
+         manyDto.value = 5000
+         const batchPayload = await service.createMany(manyDto)
+         expect(batchPayload).toEqual({ count: 1 })
+      })
+
+      it("should create only one opened package", async () => {
+         manyDto.value = 1099
+         const batchPayload = await service.createMany(manyDto)
+         expect(batchPayload).toEqual({ count: 1 })
+      })
+
+      it("should create many closed packages", async () => {
+         manyDto.value = 15000
+         const batchPayload = await service.createMany(manyDto)
+         expect(batchPayload).toEqual({ count: 3 })
+      })
+
+      it("should create many closed packages with last opened", async () => {
+         manyDto.grandpaId, (grandpaOperationId = 2)
+         manyDto.value = 14999
+         const batchPayload = await service.createMany(manyDto)
+
+         expect(batchPayload).toEqual({ count: 3 })
+      })
+   })
+
+   describe("findAll", () => {
+      it("should find all children from operation", async () => {
+         // const children = await service.findAll(operationId, true)
+         // expect(children).toHaveLength(2)
+      })
+
+      it("should find all children from grandpa operation", async () => {
+         // const children = await service.findAll(grandpaOperationId)
+         // expect(children).toHaveLength(3)
+         // expect(children[2]).toMatchObject({ status: 'opened'})
+      })
    })
 })
--- src/package/package.service.ts
+++ src/package/package.service.ts
@@ -1,82 +1,106 @@
-import { CreateManyPackagesDto } from "./dto/create-many-packages.dto";
-import { PrismaService } from "./../prisma/prisma.service";
-import { Injectable } from "@nestjs/common";
-import { CreatePackageDto, CreateNestedPackageDto } from "./dto/create-package.dto";
+import {
+   ClosedPackage,
+   OpenedPackage,
+   Package
+} from "./entities/package.entity"
+import { PrismaService } from "./../prisma/prisma.service"
+import { Injectable } from "@nestjs/common"
+import {
+   CreatePackageDto,
+   CreateNestedPackageDto
+} from "./dto/create-package.dto"
+import { CreateManyPackagesDto } from "./dto/create-many-packages.dto"
+import { maxBillQuantity } from "../constants"
+import { generateRandomHexColor } from "./utils"
 
 @Injectable()
 export class PackageService {
    constructor(private readonly prisma: PrismaService) {}
-   private readonly MAX_BILL_QUANTITY = 50;
 
-   async createMany(dto: CreateManyPackagesDto) {
-      const pkgCount = this.prisma.package.createMany()
+   /**
+    * Create many packages at once.
+    * Intended to be used to create packages for sub-operations.
+    *
+    * @param {CreateManyPackagesDto} dto Data Transfer Object
+    * @return {*} Quantity of packages created
+    * @memberof PackageService
+    */
+   async createMany(dto: CreateManyPackagesDto): Promise<{ count: number }> {
+      const data = []
+      const billQuantity = Math.floor(dto.value / dto.billType)
+      const closedPackagesQuantity = Math.floor(billQuantity / maxBillQuantity)
+      delete dto.value
+
+      for (let i = closedPackagesQuantity; i > 0; i--) {
+         data.push(new ClosedPackage({ ...dto }))
+      }
+
+      const remainingBills = Math.floor(
+         billQuantity - closedPackagesQuantity * maxBillQuantity
+      )
+      if (remainingBills) {
+         data.push(new OpenedPackage({ ...dto, billQuantity: remainingBills }))
+      }
+
+      const batchPayload = this.prisma.package.createMany({ data })
+
+      return batchPayload
    }
 
-   create(dto: CreatePackageDto) {
-      const status = dto.billQuantity < this.MAX_BILL_QUANTITY ? 'opened' : 'closed'
-
+   async create(dto: CreatePackageDto) {
       const pkg = this.prisma.package.create({
-         data: {
-            ...dto,
-            status,
-            color: this.generateRandomHexColor(),
-         },
-      });
+         data: new Package({ ...dto })
+      })
 
-      return pkg;
+      return pkg
    }
 
-   findAll(operationId: number) {
-      return this.prisma.package.findMany({
-         where: { operationId },
-      });
+   async findAll(operationId: number, grandpa?: true) {
+      const where = grandpa ? { grandpaId: operationId } : { operationId }
+
+      return this.prisma.package.findMany({ where })
    }
 
    findOne(packageId: number, operationId: number) {
       return this.prisma.package.findFirst({
-         where: { id: packageId, operationId },
-      });
+         where: { id: packageId, operationId }
+      })
    }
 
    remove(packageId: number) {
       return this.prisma.package.delete({
-         where: { id: packageId },
-      });
+         where: { id: packageId }
+      })
    }
 
    nestedCreateMany(args: { value: number; billType: number }) {
-      const packages: CreateNestedPackageDto[] = [];
-      const remainingBills = Math.round(args.value % args.billType);
-      let closedPackages = Math.round(args.value / args.billType);
+      const packages: CreateNestedPackageDto[] = []
+      const remainingBills = Math.round(args.value % args.billType)
+      let closedPackages = Math.round(args.value / args.billType)
 
       while (closedPackages) {
          const closedPackage = {
-            billQuantity: this.MAX_BILL_QUANTITY,
+            billQuantity: maxBillQuantity,
             status: "closed",
-            color: this.generateRandomHexColor(),
+            color: generateRandomHexColor(),
             billType: args.billType
          }
 
-         packages.push(closedPackage);
-         closedPackages--;
+         packages.push(closedPackage)
+         closedPackages--
       }
 
       if (remainingBills) {
          const openedPackage = {
             billQuantity: remainingBills,
             status: "opened",
-            color: this.generateRandomHexColor(),
+            color: generateRandomHexColor(),
             billType: args.billType
          }
 
          packages.push(openedPackage)
       }
 
-      return packages;
-   }
-
-   generateRandomHexColor(): string {
-      const n = (Math.random() * 0xfffff * 1000000).toString(16);
-      return "#" + n.slice(0, 6);
+      return packages
    }
 }
